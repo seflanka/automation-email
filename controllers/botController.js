330f0018ad2824b47834958271af5bc3e5a1b56e
Guilherme de  Souza <91706877+seflanka@users.noreply.github.com>
2022-11-30 15:37:10 -0300

    //console.log()

--- controllers/botController.js
+++ controllers/botController.js
@@ -45,7 +45,7 @@ module.exports = class automation {
     
             const emailClick = ($1('#mail-app-component > div > div > div > div.D_F.ab_FT.em_N.ek_BB.iz_A.H_6D6F > div > div > div.W_6D6F.H_6D6F.cZ1RN91d_n.o_h.p_R.em_N.D_F > div > div.p_R.Z_0.iy_h.iz_A.W_6D6F.H_6D6F.k_w.em_N.c22hqzz_GN > ul > li:nth-child(3)')).text()
              
-            console.log(emailClick.length)
+            // console.log(emailClick.length)
     
             if (emailClick.length == 0 ) {
                 return res.status(200).json( { message: 'Acabou a leitura' } )
